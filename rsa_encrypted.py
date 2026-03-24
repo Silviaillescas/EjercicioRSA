87b89bbffc2bc5f0bab92db3cb16e0d4d9619df2
Silviaillescas <silviaillesca402@gmail.com>
2026-03-24 16:09:50 -0600

    Implementar cifrado y descifrado RSA-OAEP

---
+++ rsa_encrypted.py
@@ -0,0 +1,14 @@
+from Crypto.PublicKey import RSA
+from Crypto.Cipher import PKCS1_OAEP
+
+mensaje = b"secreto secreto"
+
+# cargar clave pública
+with open("public_key.pem", "rb") as f:
+    public_key = RSA.import_key(f.read())
+
+cipher = PKCS1_OAEP.new(public_key)
+
+ciphertext = cipher.encrypt(mensaje)
+
+print("Ciphertext:", ciphertext)
